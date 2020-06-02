c67171d30564cc7e2d25fe2ba51f5aabad887859
Felipe Merchán <felipe-sc@hotmail.es>
2020-06-01 23:32:21 -0500

    Terminado el segundo módulo de fundamentos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const container = document.getElementById('app');
+/* const jsx = <h1>Hello, Platzi Badges from React</h1> */
+/* const element = React.createElement(
+  'a', 
+  { href: 'https://platzi.com/' }, 
+  'Ir a Platzi'
+  ) */
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+const name = 'Felipe'
+/* const element = React.createElement('h1', {}, `Hola, soy ${name}`) */
+
+const jsx = (
+  <div>
+    <h1>Hola. Soy Felipe</h1>
+    <p>Soy Arquitecto frontend</p>
+  </div>
+)
+
+const container = document.getElementById('app')
+
+ReactDOM.render(jsx, container)
