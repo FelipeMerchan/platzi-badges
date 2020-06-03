e8bacdb34be612c3ac2a102afbc11f17c4277c27
Felipe Merchán <felipe-sc@hotmail.es>
2020-06-03 00:24:24 -0500

    Ejercicio de las clases: Cómo aplicar esetilos y Proops

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import './global.css'
 import Badge from './components/Badge'
 
 const container = document.getElementById('app')
 
-ReactDOM.render(<Badge />, container)
+ReactDOM.render(
+  <Badge
+    firstName="Felipe"
+    lastName="Merchan"
+    avatarUrl="https://1.gravatar.com/avatar/747c9ab5796cdc4d11bdc15d4814d0a1s"
+    jobTitle="Frontend Arquitect"
+    twitter="felipe___sc"
+  />,
+  container)
