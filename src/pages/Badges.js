57ff5a5e0d1a80e4617137d469a9b2f457221f40
Felipe Merchan <felipe-sc@hotmail.es>
2020-09-22 23:11:24 -0500

    Responsive was made for the pages: Bages and BadgeNew.

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -50,7 +50,7 @@ class Badges extends React.Component {
         <div className="Badges">
           <div className="Badges__hero">
             <div className="Badges__container">
-              <img className="Badges_conf-logo" src={confLogo} alt="Conf Logo" />
+              <img className="Badges_conf-logo" src={confLogo} alt="Conf Logo" width="190" />
             </div>
           </div>
         </div>
