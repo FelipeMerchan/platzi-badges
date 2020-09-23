f012bc0eeebbec17d25f49480a13671800c7a4ee
Felipe Merchan <felipe-sc@hotmail.es>
2020-09-22 23:32:52 -0500

    Responsive was made for the following pages: BadgeEdit and BadgeDetails.

--- src/components/PageError.js
+++ src/components/PageError.js
@@ -8,7 +8,7 @@ function PageError(props) {
   return (
     <div className="PageError">
       <div className="PageError__image__container">
-        <img className="img-fluid" src={typeError} />
+        <img className="img-fluid" src={typeError} alt="Servicio no disponible"/>
       </div>
       <Link className="btn btn-primary" to="/" >
         Volver al Home
